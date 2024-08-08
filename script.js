dde7b89d09b3c17a8651cc61fed3877854b212ba
pedro eugelmi <pedrogabrielmendy@gmail.com>
2024-08-07 22:21:19 -0300

    adicionandoo perguntas frequentes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* Abrir e fechar menu
+---------------------------------*/
+
 let button = document.querySelector('.header-abrir');
 
 button.addEventListener('click', function (){ 
@@ -10,3 +13,15 @@ fecharMenu.addEventListener('click', function (){
   document.querySelector('.header-mobile').classList.remove('menu-active');
 });
 
+/* Perguntas frequentes
+---------------------------------*/
+let perguntas_triggers = document.querySelectorAll('.perguntas-item');
+console.log(perguntas_triggers);
+
+perguntas_triggers.forEach(function(trigger) {
+  trigger.addEventListener('click', function() {
+    let content = this.querySelector('.perguntas-conteudo');
+    
+    content.classList.toggle('perguntas-active');
+  });
+});
